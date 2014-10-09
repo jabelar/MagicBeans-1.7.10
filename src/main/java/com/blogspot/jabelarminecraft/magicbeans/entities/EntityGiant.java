7b8e140247a937a8f98e417a32a84981f5ce2dd6
jabelar <julian_abelar@hotmail.com>
2014-10-09 01:14:22 -0700

    Got giant attack working

--- src/main/java/com/blogspot/jabelarminecraft/magicbeans/entities/EntityGiant.java
+++ src/main/java/com/blogspot/jabelarminecraft/magicbeans/entities/EntityGiant.java
@@ -38,11 +38,14 @@ import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeHooks;
 
 import com.blogspot.jabelarminecraft.magicbeans.ai.EntityGiantAINearestAttackableTarget;
 import com.blogspot.jabelarminecraft.magicbeans.ai.EntityGiantAISeePlayer;
@@ -68,6 +71,18 @@ public class EntityGiant extends EntityCreature implements IEntityMagicBeans
     protected EntityAIBase aiHurtByTarget = new EntityAIHurtByTarget(this, true);
     protected EntityAIBase aiNearestAttackableTarget = new EntityGiantAINearestAttackableTarget(this, EntityPlayer.class, 2, true, false, (IEntitySelector)null);
     protected EntityAIBase aiSeePlayer = new EntityGiantAISeePlayer(this, 16.0D);
+
+    // fields related to being attacked
+    protected Entity entityAttackedBy = null;
+    protected boolean wasDamageDoneOutsideResistancePeriod = true;
+    protected DamageSource damageSource = null;
+    protected float damageAmount = 0;
+    
+    // fields related to attacking
+    protected Entity entityAttacked = null;
+    protected float attackDamage = 1.0F;
+    protected int knockback = 0;
+	protected boolean wasDamageDone = false;
     
 	/**
 	 * @param parWorld
@@ -165,42 +180,47 @@ public class EntityGiant extends EntityCreature implements IEntityMagicBeans
     @Override
 	public boolean attackEntityAsMob(Entity parEntity)
     {
-        float f = (float)getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue();
-        int i = 0;
+    	//DEBUG
+    	System.out.println("EntityGiant attackEntityAsMob");
+    	
+    	entityAttacked = parEntity;
+        attackDamage = (float)getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue();
+        knockback = 0;
 
-        if (parEntity instanceof EntityLivingBase)
+        if (entityAttacked instanceof EntityLivingBase)
         {
-            f += EnchantmentHelper.getEnchantmentModifierLiving(this, (EntityLivingBase)parEntity);
-            i += EnchantmentHelper.getKnockbackModifier(this, (EntityLivingBase)parEntity);
+            attackDamage += EnchantmentHelper.getEnchantmentModifierLiving(this, (EntityLivingBase)entityAttacked);
+            knockback += EnchantmentHelper.getKnockbackModifier(this, (EntityLivingBase)entityAttacked);
         }
 
-        boolean flag = parEntity.attackEntityFrom(DamageSource.causeMobDamage(this), f);
-
-        if (flag)
+        wasDamageDone = entityAttacked.attackEntityFrom(DamageSource.causeMobDamage(this), attackDamage);
+        // DEBUG
+        System.out.println("Damage was done ="+wasDamageDone+", damage amount ="+attackDamage);
+        if (wasDamageDone)
         {
-            if (i > 0)
+            if (knockback > 0)
             {
-                parEntity.addVelocity(-MathHelper.sin(rotationYaw * (float)Math.PI / 180.0F) * i * 0.5F, 0.1D, MathHelper.cos(rotationYaw * (float)Math.PI / 180.0F) * i * 0.5F);
+            	entityAttacked.addVelocity(-MathHelper.sin(rotationYaw * (float)Math.PI / 180.0F) * knockback * 0.5F, 0.1D, MathHelper.cos(rotationYaw * (float)Math.PI / 180.0F) * knockback * 0.5F);
                 motionX *= 0.6D;
                 motionZ *= 0.6D;
             }
 
-            int j = EnchantmentHelper.getFireAspectModifier(this);
+            int fireModifier = EnchantmentHelper.getFireAspectModifier(this);
 
-            if (j > 0)
+            if (fireModifier > 0)
             {
-                parEntity.setFire(j * 4);
+            	entityAttacked.setFire(fireModifier * 4);
             }
 
-            if (parEntity instanceof EntityLivingBase)
+            if (entityAttacked instanceof EntityLivingBase)
             {
-                EnchantmentHelper.func_151384_a((EntityLivingBase)parEntity, this);
+                EnchantmentHelper.func_151384_a((EntityLivingBase)entityAttacked, this);
             }
 
-            EnchantmentHelper.func_151385_b(this, parEntity);
+            EnchantmentHelper.func_151385_b(this, entityAttacked);
         }
 
-        return flag;
+        return wasDamageDone;
     }
 
     /**
@@ -209,32 +229,212 @@ public class EntityGiant extends EntityCreature implements IEntityMagicBeans
     @Override
 	public boolean attackEntityFrom(DamageSource parDamageSource, float parDamageAmount)
     {
-        if (isEntityInvulnerable())
-        {
-            return false;
-        }
-        else if (super.attackEntityFrom(parDamageSource, parDamageAmount))
-        {
-            Entity entity = parDamageSource.getEntity();
+    	damageSource = parDamageSource;
+    	damageAmount = parDamageAmount;
+        entityAttackedBy = damageSource.getEntity();
 
-            if (riddenByEntity != entity && ridingEntity != entity)
+        // DEBUG
+    	System.out.println("EntityGiant attackEntityFrom()");
+        if (ForgeHooks.onLivingAttack(this, damageSource, damageAmount)) return false;
+
+        if (conditionsPreventDamage(damageSource))
+        {
+        	return false;
+        }
+
+        wasDamageDoneOutsideResistancePeriod = processDamage(damageSource, damageAmount);
+               
+        updateEntityState();
+                
+        // process death
+        if (getHealth() <= 0.0F)
+        {
+        	onDeath(damageSource);
+        }
+        
+        playHurtOrDeathSound();
+
+		return wasDamageDoneOutsideResistancePeriod;
+    }
+
+	private void updateEntityState() 
+	{
+        // flail limbs when attacked
+        limbSwingAmount = 1.5F;
+
+        entityAge = 0; // reset despawn counter if you've been attacked by something
+
+        if (entityAttackedBy != null)
+        {
+        	// DEBUG
+        	System.out.println("Attacked by an entity");
+            if (entityAttackedBy instanceof EntityLivingBase)
             {
-                if (entity != this)
+                setRevengeTarget((EntityLivingBase)entityAttackedBy);
+            }
+
+            if (entityAttackedBy instanceof EntityPlayer)
+            {
+            	// DEBUG
+            	System.out.println("Attacked by a player");
+                recentlyHit = 100;
+                attackingPlayer = (EntityPlayer)entityAttackedBy;
+            }
+            else if (entityAttackedBy instanceof EntityTameable)
+            {
+                EntityTameable entityTameable = (EntityTameable)entityAttackedBy;
+
+                if (entityTameable.isTamed())
                 {
-                    entityToAttack = entity;
+                    recentlyHit = 100;
+                    attackingPlayer = null;
+                }
+            }
+        }
+
+        if (wasDamageDoneOutsideResistancePeriod)
+        {
+            worldObj.setEntityState(this, (byte)2);
+
+            if (damageSource != DamageSource.drown)
+            {
+                setBeenAttacked();
+            }
+
+            if (entityAttackedBy != null)
+            {
+                double d1 = entityAttackedBy.posX - posX;
+                double d0;
+
+                for (d0 = entityAttackedBy.posZ - posZ; d1 * d1 + d0 * d0 < 1.0E-4D; d0 = (Math.random() - Math.random()) * 0.01D)
+                {
+                    d1 = (Math.random() - Math.random()) * 0.01D;
                 }
 
-                return true;
+                attackedAtYaw = (float)(Math.atan2(d0, d1) * 180.0D / Math.PI) - rotationYaw;
+                knockBack(entityAttackedBy, damageAmount, d1, d0);
             }
-            else
+            else // non-entity was damage source
             {
-                return true;
+            	// DEBUG
+            	System.out.println("Damage was done by something other than an entity");
+                attackedAtYaw = (int)(Math.random() * 2.0D) * 180;
             }
+        }
+        
+        if (riddenByEntity != entityAttackedBy && ridingEntity != entityAttackedBy)
+          {
+              if (entityAttackedBy != this)
+              {
+                  entityToAttack = entityAttackedBy;
+              }
+          }
+	}
+
+	/**
+	 * @param wasDamageDoneOutsideResistancePeriod
+	 */
+	private void playHurtOrDeathSound() 
+	{
+        String soundName;
+
+        if (getHealth() <= 0.0F)
+        {
+            soundName = getDeathSound();
         }
         else
         {
-            return false;
+        	soundName = getHurtSound();
         }
+
+        if (wasDamageDoneOutsideResistancePeriod && soundName != null)
+        {
+            playSound(soundName, getSoundVolume(), getSoundPitch());
+        }
+	}
+
+	/**
+	 * @param parDamageSource
+	 * @param parDamageAmount
+	 * @return
+	 */
+	private boolean processDamage(DamageSource parDamageSource,
+			float parDamageAmount) 
+	{       
+        // Check if helmet protects from anvil or falling block, and damage helmet
+        if ((damageSource == DamageSource.anvil || damageSource == DamageSource.fallingBlock) && getEquipmentInSlot(4) != null)
+        {
+            getEquipmentInSlot(4).damageItem((int)(damageAmount * 4.0F + rand.nextFloat() * damageAmount * 2.0F), this);
+            damageAmount *= 0.75F;
+        }
+
+        wasDamageDoneOutsideResistancePeriod = true;
+
+        // Check for hurt resistance
+        if (hurtResistantTime > maxHurtResistantTime / 2.0F)
+        {
+        	// DEBUG
+        	System.out.println("Reduced damage done, damage amount ="+damageAmount+". health remaining ="+getHealth());
+            if (damageAmount <= lastDamage)
+            {
+                return false;
+            }
+
+            damageEntity(damageSource, damageAmount - lastDamage);
+            lastDamage = damageAmount;
+            wasDamageDoneOutsideResistancePeriod = false;
+        }
+        else // do normal damage
+        {
+        	// DEBUG
+        	System.out.println("Normal damage done, damage amount ="+damageAmount+". health remaining ="+getHealth());
+            lastDamage = damageAmount;
+            prevHealth = getHealth();
+            hurtResistantTime = maxHurtResistantTime;
+            damageEntity(damageSource, damageAmount);
+            hurtTime = maxHurtTime = 10;
+        }
+        
+		return wasDamageDoneOutsideResistancePeriod;
+	}
+
+	/**
+	 * @param parDamageSource
+	 * @return
+	 */
+	private boolean conditionsPreventDamage(DamageSource parDamageSource) 
+	{
+        // Only process damage on server side
+        if (worldObj.isRemote)
+        {
+            return true;
+        }
+
+        // Check for invlunerability
+        if (isEntityInvulnerable())
+        {
+            return true;
+        }
+
+        // Check if already dead
+        if (getHealth() <= 0.0F)
+        {
+            return true;
+        }
+
+        // Check if damage is from fire but resistant
+        if (damageSource.isFireDamage() && isPotionActive(Potion.fireResistance))
+        {
+            return true;
+        }
+        
+		return false;
+	}
+    
+    @Override
+	public boolean allowLeashing()
+    {
+    	return false;
     }
 
 	/* (non-Javadoc)
